350321bff272ecfb31f8540e5c2fb8283694eeb5
Rajinder Singh <raj4all42@gmail.com>
2022-09-03 14:12:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,12 +51,15 @@ Here's our first attempt at using data to create a table:
 
 # df
 
-import streamlit as st
-import numpy as np
-import pandas as pd
-
 dataframe = pd.DataFrame(
     np.random.randn(10, 20),
     columns=('col %d' % i for i in range(20)))
 
 st.dataframe(dataframe.style.highlight_max(axis=0))
+
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
