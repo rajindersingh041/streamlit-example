3b93d91b80a116ac8a48f7ab44d1230d740a0075
Rajinder Singh <raj4all42@gmail.com>
2022-09-05 07:59:45 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,4 +13,33 @@ chart_data = pd.DataFrame(
      np.random.randn(20, 1),
      columns=['a'])
 
-st.area_chart(chart_data, color = 'green')
+series = pd.DataFrame({
+  'year': ['2010', '2011', '2012', '2013','2010', '2011', '2012', '2013'],
+  'animal': ['antelope', 'antelope', 'antelope', 'antelope', 'velociraptor', 'velociraptor', 'velociraptor', 'velociraptor',],
+  'count': [8, 6, 3, 1, 2, 4, 5, 5]
+})
+
+# Basic Altair line chart where it picks automatically the colors for the lines
+basic_chart = alt.Chart(series).mark_line().encode(
+    x='year',
+    y='count',
+    color='animal',
+    # legend=alt.Legend(title='Animals by year')
+)
+
+# Custom Altair line chart where you set color and specify dimensions
+custom_chart = alt.Chart(series).mark_line().encode(
+    x='year',
+    y='count',
+    color=alt.Color('animal',
+            scale=alt.Scale(
+                domain=['antelope', 'velociraptor'],
+                range=['blue', 'red'])
+                )
+).properties(
+    width=900,
+    height=500
+)
+
+st.altair_chart(basic_chart)
+st.altair_chart(custom_chart)
