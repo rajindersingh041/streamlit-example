fd982b80461b0ad0cfb693e8df3c67b034035005
Rajinder Singh <raj4all42@gmail.com>
2022-09-05 17:14:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 import plotly.graph_objs as go
-# import plotly.express as px
+import plotly.express as px
 """
 ## Welcome to Rajinder Singh Algo engine tool 2!
 
