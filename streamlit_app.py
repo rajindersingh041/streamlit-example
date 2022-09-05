1848b862f33ac9998b1a3dd12c298d2f3928d0d7
Rajinder Singh <raj4all42@gmail.com>
2022-09-05 08:04:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,21 @@ custom_chart = alt.Chart(series).mark_line().encode(
 
 st.altair_chart(basic_chart)
 st.altair_chart(custom_chart)
+
+import altair as alt
+import pandas as pd
+import numpy as np
+
+x = np.linspace(0, 100, 1000)
+y = np.sin(x)
+df = pd.DataFrame({'x': x, 'y': y})
+
+t = alt.Chart(df).transform_calculate(
+    negative='datum.y < 0'
+).mark_area().encode(
+    x='x',
+    y=alt.Y('y', impute={'value': 0}),
+    color='negative:N'
+)
+
+st.altair_chart(t)
